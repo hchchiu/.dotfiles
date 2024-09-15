c2d6d6ff4a4ff62a81702845cbaf55918966bfb8
hchchiu <robert890619@gmail.com>
2024-09-16 00:15:10 +0800

    external screen config

---
+++ screenlayout/.screenlayout/rog_xg279q.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output HDMI-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off
